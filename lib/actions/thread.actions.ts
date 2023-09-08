b25e09928c4c382b0b7fa735f265f7519eab9c87
katsuro666 <kreepinoutdakut@gmail.com>
2023-09-08 17:52:41 +0300

    prepare a template for Threads on the home page

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,4 +1,4 @@
-'use server'
+'use server';
 
 import { connectToDb } from '../mongoose';
 import Thread from '../models/thread.model';
@@ -24,4 +24,34 @@ export async function createThread({ text, author, communityId, path }: Params) 
   } catch (error: any) {
     throw new Error(`Failed to create thread: ${error.message}`);
   }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDb();
+
+  // calculate the number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // fetch the posts that have no parents (comments)
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: 'author', model: User })
+    .populate({
+      path: 'children',
+      populate: {
+        path: 'author',
+        model: User,
+        select: '_id name parentId image',
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } });
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
 }
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -6,7 +6,8 @@ export const connectToDb = async () => {
   mongoose.set('strictQuery', true);
 
   if (!process.env.MONGODB_URL) return console.log('MONGODB_URL not found');
-  if (!isConnected) return console.log('Already connected to MongoDB');
+  // if (!isConnected) return console.log('Already connected to MongoDB');
+  if (!isConnected) console.log('Already connected to MongoDB');
 
   try {
     await mongoose.connect(process.env.MONGODB_URL);
