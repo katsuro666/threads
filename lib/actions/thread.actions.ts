167002f5fc99ab4505fb11f53936a613c55e35f5
katsuro666 <kreepinoutdakut@gmail.com>
2023-09-11 16:44:47 +0300

    Add the ability to open each Thread. For now without the ability to view or write comments

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -55,3 +55,40 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
 
   return { posts, isNext };
 }
+
+export async function fetchThreadById(threadId: string) {
+  connectToDb();
+
+  try {
+    const thread = await Thread.findById(threadId)
+      .populate({
+        path: 'author',
+        model: User,
+        select: '_id id name image',
+      })
+      .populate({
+        path: 'children',
+        populate: [
+          {
+            path: 'author',
+            model: User,
+            select: '_id id name parentId image',
+          },
+          {
+            path: 'children',
+            model: Thread,
+            populate: {
+              path: 'author',
+              model: User,
+              select: '_id id name parentId image',
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch thread: ${error.message}`);
+  }
+}
