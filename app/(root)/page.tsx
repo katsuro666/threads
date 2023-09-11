98a9056e1240fa71137a0a5b128ee203fafcd7b0
katsuro666 <kreepinoutdakut@gmail.com>
2023-09-11 16:20:48 +0300

    complete the ThreadCard view for the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,10 @@
+import ThreadCard from '@/components/cards/ThreadCard';
 import { fetchPosts } from '@/lib/actions/thread.actions';
 import { currentUser } from '@clerk/nextjs';
 
 export default async function Home() {
   const user = await currentUser();
   const result = await fetchPosts(1, 30);
-
-  console.log(result);
 
   return (
     <>
@@ -20,7 +19,7 @@ export default async function Home() {
               <ThreadCard
                 key={post._id}
                 id={post._id}
-                currentUserId={user?.id}
+                currentUserId={user?.id || ''}
                 parentId={post.parentId}
                 content={post.text}
                 author={post.author}
