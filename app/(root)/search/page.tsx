13c2e1e14b32dec6474b24eb4f671a35b937c7bb
katsuro666 <kreepinoutdakut@gmail.com>
2023-09-14 17:18:25 +0300

    make the Activity page work

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,10 +1,6 @@
 import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
-import ProfileHeader from '@/components/shared/ProfileHeader';
-import { profileTabs } from '@/constants';
-import Image from 'next/image';
-import ThreadsTab from '@/components/shared/ThreadsTab';
 import UserCard from '@/components/cards/UserCard';
 
 const Page = async () => {
